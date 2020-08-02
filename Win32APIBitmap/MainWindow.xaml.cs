1ad5ed9d89770319c4153427ea159abdf4f5b216
nonnon <nonnon@mail.com>
2020-08-02 17:42:48 +0900

    上下反転処理、32bit変換処理追加、24bit保存処理追加

--- Win32APIBitmap/MainWindow.xaml.cs
+++ Win32APIBitmap/MainWindow.xaml.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * 補足：
+ * test1.bmp    1920x1080   24bit
+ * test2.bmp    11520x6480  24bit
+ * 
+ * それぞれ速度計測
+ */ 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,6 +33,7 @@ namespace Win32APIBitmap
             InitializeComponent();
         }
 
+        #region "定義"
         //https://docs.microsoft.com/en-us/windows/win32/api/wingdi/ns-wingdi-bitmapfileheader
         [StructLayout(LayoutKind.Sequential,Pack = 1)]
         struct BITMAPFILEHEADER
@@ -113,7 +121,29 @@ namespace Win32APIBitmap
         [DllImport("gdi32.dll", ExactSpelling = true, SetLastError = true)]
         static extern IntPtr CreateCompatibleBitmap(IntPtr hdC,int cx,int cy);
 
-        //TEST
+        public class TM
+        {
+            private static System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+            public static void start()
+            {
+                sw.Restart();
+            }
+
+            public static void show()
+            {
+                Console.WriteLine("{0:0.000}", sw.Elapsed.TotalSeconds);
+            }
+
+            public static void stop()
+            {
+                sw.Stop();
+                Console.WriteLine("{0:0.000}", sw.Elapsed.TotalSeconds);
+            }
+        }
+        #endregion
+
+        //TEST1
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
             string fname = @"D:\movie\壁紙\test.bmp";
@@ -155,7 +185,7 @@ namespace Win32APIBitmap
 
         }
 
-        //Bitmap→BitmapSource
+        //TEST2 Bitmap→BitmapSource
         private void MenuItem_Click_1(object sender, RoutedEventArgs e)
         {
             string fname = @"D:\movie\壁紙\test.bmp";
@@ -164,13 +194,13 @@ namespace Win32APIBitmap
             Img1.Source = image;
         }
 
-        //FileStreamとBITMAPINFOHEADERを使ってDIB画像を直接読み込む(0.0120069)
+        //TEST3 FileStreamとBITMAPINFOHEADERを使ってDIB画像を直接読み込む(0.0120069)(2.2738832)
         private void MenuItem_Click_2(object sender, RoutedEventArgs e)
         {
             System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
             sw.Start();
 
-            string fname = @"D:\movie\壁紙\test.bmp";
+            string fname = @"D:\movie\壁紙\test2.bmp";
             var fs = new FileStream(fname, FileMode.Open);
 
             var reader = new BinaryReader(fs);
@@ -194,6 +224,8 @@ namespace Win32APIBitmap
 
             tmpptr = IntPtr.Add(tmpptr, Marshal.SizeOf(iheader));
 
+            ChangeUpDown(tmpptr, iheader);
+
             int stride = iheader.biWidth * (iheader.biBitCount / 8);
 
             var src = BitmapSource.Create(iheader.biWidth, iheader.biHeight, iheader.biXPelsPerMeter, iheader.biYPelsPerMeter,
@@ -205,7 +237,7 @@ namespace Win32APIBitmap
             Console.WriteLine(sw.Elapsed.TotalSeconds.ToString());
         }
 
-        //FileStreamとBITMAPINFOHEADERを使ってDIB画像を保存する(0.0111148)
+        //TEST4 FileStreamとBITMAPINFOHEADERを使ってDIB画像を保存する(0.0111148)(1.6200039)
         private void MenuItem_Click_3(object sender, RoutedEventArgs e)
         {
             System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
@@ -262,7 +294,7 @@ namespace Win32APIBitmap
             Console.WriteLine(sw.Elapsed.TotalSeconds.ToString());
         }
 
-        //BmpEncoderで保存する(0.2341903)
+        //TEST5 BmpEncoderで保存する(24bitは24bit 32bitは32bitで保存)(0.2341903)(8.2563395)
         private void MenuItem_Click_4(object sender, RoutedEventArgs e)
         {
             System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
@@ -278,7 +310,7 @@ namespace Win32APIBitmap
             Console.WriteLine(sw.Elapsed.TotalSeconds.ToString());
         }
 
-        //グレースケール画像をBITMAPINFOHEADERを使って読み込む
+        //TEST6 グレースケール画像をBITMAPINFOHEADERを使って読み込む
         private void MenuItem_Click_5(object sender, RoutedEventArgs e)
         {
             string fname = @"D:\movie\壁紙\test_gray.bmp";
@@ -306,6 +338,8 @@ namespace Win32APIBitmap
             //画像オフセットの位置がカラーと違うので注意
             tmpptr = new IntPtr(ptr.ToInt32() + fheader.bfOffBits);
 
+            ChangeUpDown(tmpptr, iheader);
+
             int stride = iheader.biWidth * (iheader.biBitCount / 8);
             int size = stride * iheader.biHeight;
 
@@ -315,18 +349,227 @@ namespace Win32APIBitmap
             Img1.Source = src;
         }
 
-        //BitmapImageで画像を読み込む(0.0239597)
+        //TEST7 BitmapImageで画像を読み込む(0.0239597)(0.0245081) 読み込みはBitmapImageが最高
         private void MenuItem_Click_6(object sender, RoutedEventArgs e)
         {
             System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
             sw.Start();
 
-            string fname = @"D:\movie\壁紙\test.bmp";
+            string fname = @"D:\movie\壁紙\test2.bmp";
             BitmapImage img = new BitmapImage(new Uri(fname));
             Img1.Source = img;
 
             sw.Stop();
             Console.WriteLine(sw.Elapsed.TotalSeconds.ToString());
+        }
+
+        //上下反転処理
+        private void ChangeUpDown(IntPtr ptr,BITMAPINFOHEADER ihead)
+        {
+            int pxlsize = (ihead.biBitCount / 8);
+            int stride = ihead.biWidth * pxlsize;
+
+            IntPtr p = new IntPtr(ptr.ToInt32());
+            IntPtr p2 = new IntPtr(ptr.ToInt32() +  stride * (ihead.biHeight-1));
+
+            int harfy = ihead.biHeight / 2;
+
+            byte[] tmpb = new byte[3];
+
+            for(int y = 0; y < harfy; y++)
+            {
+                for(int x = 0; x < ihead.biWidth; x++)
+                {
+                    for(int i = 0; i < pxlsize; i++)
+                    {
+                        tmpb[i] = Marshal.ReadByte(p, i);
+                        Marshal.WriteByte(p, i, Marshal.ReadByte(p2, i));
+                        Marshal.WriteByte(p2, i, tmpb[i]);
+                    }
+                    p = IntPtr.Add(p, pxlsize);
+                    p2 = IntPtr.Add(p2, pxlsize);
+                }
+                p2 = IntPtr.Add(p2, stride * -2);
+            }
+        }
+
+        //TEST8 BITMAPINFOを使って24bit→32bitフォーマットに変換して読み込む(0.0728768)(2.4672905)
+        private void MenuItem_Click_7(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            string fname = @"D:\movie\壁紙\test2.bmp";
+            var fs = new FileStream(fname, FileMode.Open);
+
+            var reader = new BinaryReader(fs);
+
+            //ヘッダ情報読み取り
+            byte[] buf1 = reader.ReadBytes(Marshal.SizeOf<BITMAPFILEHEADER>() + Marshal.SizeOf<BITMAPINFOHEADER>());
+            IntPtr ptr = Marshal.AllocCoTaskMem(buf1.Length);
+            IntPtr tmpptr = new IntPtr(ptr.ToInt64());
+            Marshal.Copy(buf1, 0, ptr, buf1.Length);
+            BITMAPFILEHEADER fheader = Marshal.PtrToStructure<BITMAPFILEHEADER>(tmpptr);
+            tmpptr = IntPtr.Add(tmpptr, Marshal.SizeOf(fheader));
+            BITMAPINFOHEADER iheader = Marshal.PtrToStructure<BITMAPINFOHEADER>(tmpptr);
+            Marshal.FreeCoTaskMem(ptr);
+
+            //画像バッファ読み取り 24bit→32bit変換(画素を下から読み取り反転を防ぐ)
+            int pxlsize = 4;
+            int stride = iheader.biWidth * pxlsize;
+            int size = stride * iheader.biHeight;
+
+            byte[] buf2 = new byte[size];
+            for(int y = iheader.biHeight-1; y > 0 ; y--)
+            {
+                for(int x = 0; x < iheader.biWidth; x++)
+                {
+                    int pos = y * stride + (x * 4);
+
+                    buf2[pos + 0] = reader.ReadByte();
+                    buf2[pos + 1] = reader.ReadByte();
+                    buf2[pos + 2] = reader.ReadByte();
+                    buf2[pos + 3] = 0;
+                }
+            }
+
+            reader.Dispose();
+            fs.Close();
+
+            var src = BitmapSource.Create(iheader.biWidth, iheader.biHeight, 600, 600,
+                PixelFormats.Bgr32, null, buf2, stride);
+
+            Img1.Source = src;
+
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed.TotalSeconds.ToString());
+        }
+
+        //TEST9 BITMAPINFOHEADERを使ってDIB画像を保存する(32bitのまま)(1.8004037)
+        private void MenuItem_Click_8(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            BitmapSource src = Img1.Source as BitmapSource;
+
+            int stride = src.PixelWidth * (src.Format.BitsPerPixel / 8);
+
+            int size = src.PixelHeight * stride;
+
+            byte[] buf = new byte[size];
+
+            src.CopyPixels(buf, stride, 0);
+
+            string savef = "test.bmp";
+            using (var fs = new FileStream(savef, FileMode.Create))
+            using (var writer = new BinaryWriter(fs))
+            {
+                BITMAPFILEHEADER fhead = new BITMAPFILEHEADER();
+                fhead.bftype = 19778;
+                fhead.bfSize = (uint)(size + 54);
+                fhead.bfOffBits = 54;//gary →  1078
+
+                byte[] buffhead = new byte[Marshal.SizeOf(fhead)];
+                IntPtr pfhead = Marshal.AllocCoTaskMem(Marshal.SizeOf(fhead));
+                Marshal.StructureToPtr<BITMAPFILEHEADER>(fhead, pfhead, false);
+                Marshal.Copy(pfhead, buffhead, 0, buffhead.Length);
+
+                writer.Write(buffhead);
+
+                BITMAPINFOHEADER ihead = new BITMAPINFOHEADER();
+                ihead.biSize = 40;
+                ihead.biWidth = src.PixelWidth;
+                ihead.biHeight = src.PixelHeight;
+                ihead.biPlanes = 1;
+                ihead.biBitCount = (ushort)src.Format.BitsPerPixel;//gray → 8
+                ihead.biSizeImage = (uint)size;
+                ihead.biXPelsPerMeter = (int)src.DpiX;
+                ihead.biYPelsPerMeter = (int)src.DpiY;
+
+                byte[] bufihead = new byte[Marshal.SizeOf(ihead)];
+                IntPtr pihead = Marshal.AllocCoTaskMem(Marshal.SizeOf(ihead));
+                Marshal.StructureToPtr<BITMAPINFOHEADER>(ihead, pihead, false);
+                Marshal.Copy(pihead, bufihead, 0, bufihead.Length);
+
+                writer.Write(bufihead);
+
+                writer.Write(buf);
+
+            }
+
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed.TotalSeconds.ToString());
+        }
+
+        //TEST10 BITMAPINFOを使って32bit→24bitフォーマットに変換してさらに上下反転して書き込む(2.161)
+        private void MenuItem_Click_9(object sender, RoutedEventArgs e)
+        {
+            //書き込み用に24bitの画像バッファが必要になったためメモリ消費がやばそう　このPCだと余裕だが？
+
+            MenuItem_Click_6(null, null);//画像を開く
+
+            TM.start();
+
+            BitmapSource src = Img1.Source as BitmapSource;
+            int pxlsize = 4;
+            int stride = src.PixelWidth * pxlsize;
+            int size = src.PixelHeight * stride;
+
+            BITMAPFILEHEADER fhead = new BITMAPFILEHEADER();
+            fhead.bftype = 19778;
+            fhead.bfSize = (uint)(size + 54);
+            fhead.bfOffBits = 54;//gary →  1078
+            BITMAPINFOHEADER ihead = new BITMAPINFOHEADER();
+            ihead.biSize = 40;
+            ihead.biWidth = src.PixelWidth;
+            ihead.biHeight = src.PixelHeight;
+            ihead.biPlanes = 1;
+            ihead.biBitCount = 24;//gray → 8
+            ihead.biSizeImage = (uint)size;
+            ihead.biXPelsPerMeter = (int)src.DpiX;
+            ihead.biYPelsPerMeter = (int)src.DpiY;
+
+            byte[] fheadbuf = new byte[Marshal.SizeOf(fhead)];
+            GCHandle gch = GCHandle.Alloc(fheadbuf, GCHandleType.Pinned);
+            Marshal.StructureToPtr(fhead, gch.AddrOfPinnedObject(), false);
+            gch.Free();
+
+            byte[] iheadbuf = new byte[Marshal.SizeOf(ihead)];
+            gch = GCHandle.Alloc(iheadbuf, GCHandleType.Pinned);
+            Marshal.StructureToPtr(ihead, gch.AddrOfPinnedObject(), false);
+            gch.Free();
+
+            TM.show();
+
+            byte[] buf = new byte[size];
+
+            src.CopyPixels(buf, stride, 0);
+
+            TM.show();
+
+            int size24 = ihead.biHeight * (ihead.biWidth * 3);
+            byte[] wbuf = new byte[size24];
+            for (int y = 0,y2 = ihead.biHeight-1; y < ihead.biHeight; y++ , y2--)
+            {
+                for (int x = 0; x < ihead.biWidth; x++)
+                {
+                    int pos = y2 * (ihead.biWidth * 3) + (x * 3);
+                    int pos2 = y * (ihead.biWidth * 4) + (x * 4);
+
+                    wbuf[pos + 0] = buf[pos2 + 0];
+                    wbuf[pos + 1] = buf[pos2 + 1];
+                    wbuf[pos + 2] = buf[pos2 + 2];
+                }
+            }
+            using (var fs = new FileStream("test.bmp", FileMode.Create))
+            {
+                fs.Write(fheadbuf, 0, fheadbuf.Length);
+                fs.Write(iheadbuf, 0, iheadbuf.Length);
+                fs.Write(wbuf, 0, wbuf.Length);
+            }
+
+            TM.stop();
         }
     }
 }
