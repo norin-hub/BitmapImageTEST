baf5ccf38ac02011bbf571a8ae628cbeaa43e33f
農林三号 <norinsan@outlook.jp>
2021-02-19 20:10:49 +0900

    32bit変換追加

--- UnsafeBmp/MainWindow.xaml.cs
+++ UnsafeBmp/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 
 using BMP;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace UnsafeBmp
 {
@@ -99,6 +100,70 @@ namespace UnsafeBmp
                      PixelFormats.Bgr24, null, imgbuf, stride);
 
                     Img1.Source = src;
+                }
+
+                MM2.TStop();
+
+            }
+        }
+
+        //アンセーフを使って画像を開く(反転処理あり32bit変換) 測定停止 0.0895
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            MM2.TStart();
+
+            using (var fs = new FileStream(Define.fname1, FileMode.Open))
+            using (var reader = new BinaryReader(fs))
+            {
+                unsafe
+                {
+                    //BITMAPヘッダ読み込み
+                    int headersize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+                    byte[] headerbuf = reader.ReadBytes(headersize);
+
+                    BITMAPFILEHEADER* fheader;
+                    BITMAPINFOHEADER* iheader;
+
+                    fixed (byte* hbuf = headerbuf)
+                    {
+                        byte* hbuf2 = hbuf;
+                        fheader = (BITMAPFILEHEADER*)hbuf2;
+                        hbuf2 += sizeof(BITMAPFILEHEADER);
+                        iheader = (BITMAPINFOHEADER*)hbuf2;
+                    }
+
+                    //画像データ読み込み
+                    int stride = iheader->biWidth * (iheader->biBitCount / 8);
+                    int stride2 = iheader->biWidth * 4;
+
+                    int size = stride * iheader->biHeight;
+                    int size2 = stride2 * iheader->biHeight;
+
+                    IntPtr imgbufptr = Marshal.AllocCoTaskMem(size2);
+                    byte* imgbuf = (byte*)imgbufptr;
+
+                    byte[] tmpbuf = new byte[3];
+        
+                    for(int y=iheader->biHeight - 1; y >= 0; y--)
+                    {
+                        for(int x = 0; x < iheader->biWidth; x++)
+                        {
+                            fs.Read(tmpbuf, 0, 3);
+                            int pos = (y * stride2) + (x * 4);
+
+                            imgbuf[pos] = tmpbuf[0];
+                            imgbuf[pos+1] = tmpbuf[1];
+                            imgbuf[pos+2] = tmpbuf[2];
+                            imgbuf[pos+3] = 0;
+                        }
+                    }
+
+                    var src = BitmapSource.Create(iheader->biWidth, iheader->biHeight, iheader->biXPelsPerMeter, iheader->biYPelsPerMeter,
+                     PixelFormats.Bgr32, null, (IntPtr)imgbuf, size2, stride2);
+
+                    Img1.Source = src;
+
+                    Marshal.FreeCoTaskMem(imgbufptr);
                 }
 
                 MM2.TStop();
