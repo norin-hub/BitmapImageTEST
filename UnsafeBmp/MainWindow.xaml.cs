1638606a6c7d33953899eeb672a551e351dfd259
農林三号 <norinsan@outlook.jp>
2021-02-20 17:26:24 +0900

    高速書き込み追加

--- UnsafeBmp/MainWindow.xaml.cs
+++ UnsafeBmp/MainWindow.xaml.cs
@@ -31,7 +31,7 @@ namespace UnsafeBmp
         {
             MM2.TStart();
 
-            using (var fs = new FileStream(Define.fname1, FileMode.Open))
+            using (var fs = new FileStream("test.bmp", FileMode.Open))
             using (var reader = new BinaryReader(fs))
             {
                 byte[] buf = reader.ReadBytes((int)fs.Length);
@@ -169,6 +169,140 @@ namespace UnsafeBmp
                 MM2.TStop();
 
             }
+        }
+
+        //保存24bit変換反転(アンセーフ)測定停止 0.0537
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+            BitmapSource bmp = Img1.Source as BitmapSource;
+            if(bmp == null)
+            {
+                MessageBox.Show("画像が開かれていません");
+                return;
+            }
+
+            int stride = bmp.PixelWidth * 4;
+            int size = stride * bmp.PixelHeight;
+            byte[] ImgBuf = new byte[size];
+            bmp.CopyPixels(ImgBuf, stride, 0);
+
+            //Buffer.BlockCopy(ImgBuf, 0, ImgBuf, stride, size);
+
+            MM2.TStart();
+
+            string newfname = "test.bmp";
+
+            int stride2 = bmp.PixelWidth * 3;
+            int size2 = stride2 * bmp.PixelHeight;
+            BITMAPFILEHEADER fhead = new BITMAPFILEHEADER();
+            fhead.bftype = 19778;
+            fhead.bfReserved1 = 0;
+            fhead.bfReserved2 = 0;
+            fhead.bfOffBits = 54;
+            fhead.bfSize = (uint)(fhead.bfOffBits + size2);
+            BITMAPINFOHEADER ihead = new BITMAPINFOHEADER();
+            ihead.biSize = (uint)Marshal.SizeOf<BITMAPINFOHEADER>();
+            ihead.biWidth = bmp.PixelWidth;
+            ihead.biHeight = bmp.PixelHeight;
+            ihead.biPlanes = 1;
+            ihead.biBitCount = 24;
+            ihead.biSizeImage = (uint)size2;
+            ihead.biXPelsPerMeter = 3780;
+            ihead.biYPelsPerMeter = 3780;
+
+            byte[] ImgBuf2 = new byte[fhead.bfSize];
+            using (var fs = new FileStream(newfname, FileMode.Create))
+            {
+                unsafe
+                {
+                    fixed (byte* pImgBuf = ImgBuf)
+                    {
+                        fixed (byte* pImgBuf2 = ImgBuf2)
+                        {
+                            byte* ibuf = pImgBuf;
+                            byte* ibuf2 = pImgBuf2;
+                            byte* tmpibuf;
+
+                            *((BITMAPFILEHEADER*)ibuf2) = fhead;
+                            ibuf2 += sizeof(BITMAPFILEHEADER);
+                            *((BITMAPINFOHEADER*)ibuf2) = ihead;
+                            ibuf2 += sizeof(BITMAPINFOHEADER);
+
+                            //for (int y = 0; y < bmp.PixelHeight; y++)
+                            for (int y = bmp.PixelHeight-1; y >= 0; y--)
+                            {
+                                tmpibuf = ibuf + (y * stride);
+                                for (int x = 0; x < bmp.PixelWidth; x++)
+                                {
+                                    *ibuf2 = *tmpibuf;
+                                    ibuf2++;tmpibuf++;
+                                    *ibuf2 = *tmpibuf;
+                                    ibuf2++; tmpibuf++;
+                                    *ibuf2 = *tmpibuf;
+                                    ibuf2++; tmpibuf++;
+                                    tmpibuf++;
+                                }
+                            }
+                        }
+                    }
+
+                    //writer.Write(ImgBuf);
+                    fs.Write(ImgBuf2, 0, (int)fhead.bfSize);
+                }
+                ImgBuf = null;
+            }
+            //Marshal.FreeCoTaskMem(ptr);
+            MM2.TStop();
+        }
+
+        //保存 BitmapSource 測定停止 0.2660
+        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        {
+            BitmapSource bmp = Img1.Source as BitmapSource;
+            if (bmp == null)
+            {
+                MessageBox.Show("画像が開かれていません");
+                return;
+            }
+
+            MM2.TStart();
+
+            int stride = bmp.PixelWidth * 4;
+            int size = stride * bmp.PixelHeight;
+            byte[] ImgBuf = new byte[size];
+            bmp.CopyPixels(ImgBuf, stride, 0);
+
+            int stride2 = bmp.PixelWidth * 3;
+            int size2 = stride2 * bmp.PixelHeight;
+            byte[] ImgBuf2 = new byte[size2];
+
+            int pos1 = 0;
+            int pos2 = 0;
+            for (int y = bmp.PixelHeight - 1; y >= 0; y--)
+            {
+                //pos1 = (y * stride);
+                for (int x = 0; x < bmp.PixelWidth; x++)
+                {
+                    ImgBuf2[pos2] = ImgBuf[pos1];
+                    ImgBuf2[pos2+1] = ImgBuf[pos1+1];
+                    ImgBuf2[pos2+2] = ImgBuf[pos1+2];
+
+                    pos1 += 4;
+                    pos2 += 3;
+                }
+            }
+
+            var bmp2 = BitmapSource.Create(bmp.PixelWidth, bmp.PixelHeight, 600, 600, PixelFormats.Bgr24, null, ImgBuf2, stride2);
+
+
+            using (var fs = new FileStream("test1.bmp", FileMode.Create))
+            {
+                var enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bmp2));
+                enc.Save(fs);
+            }
+
+            MM2.TStop();
         }
     }
 }
